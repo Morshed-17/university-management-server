f24dd22ef0564f89ed95a71bd9af1b9128b2b50c
Morshed-17 <morshedmto@gmail.com>
2024-05-28 21:13:13 +0600

    academicFaculty files created

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -11,14 +11,28 @@ const findLastStudentId = async () => {
       _id: 0,
     },
   )
-  .sort({createdAt: -1})
-  .lean();
-  return lastStudnet?.id ? lastStudnet.id.substring(6) : undefined;
+    .sort({ createdAt: -1 })
+    .lean();
+  return lastStudnet?.id ? lastStudnet.id : undefined;
 };
 
 //  year semesterCode 4 digit number
 export const generateStudentId = async (payLoad: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); // 0000
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(0, 4); //01
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+
+  const currentSemesterCode = payLoad.code;
+  const currentYear = payLoad.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let increamentId = (Number(currentId) + 1).toString().padStart(4, '0');
   increamentId = `${payLoad.year}${payLoad.code}${increamentId}`;
   return increamentId;
