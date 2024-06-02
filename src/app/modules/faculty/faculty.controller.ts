43e9db4cac631f4ae4c9cdefd8daadc0864c470a
Morshed-17 <morshedmto@gmail.com>
2024-06-02 16:46:56 +0600

    some changes

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -3,50 +3,57 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { FacultyServices } from './faculty.service';
 
-const getAllFaculties = catchAsync(async (req, res) => {
-  const result = await FacultyServices.getAllFacultiesFromDB();
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Faculties retrieved successfully',
-    data: result,
-  });
-});
 const getSingleFaculty = catchAsync(async (req, res) => {
-  const { facultyId } = req.params;
-  const result = await FacultyServices.getSingleFacultyFromDB(facultyId);
+  const { id } = req.params;
+  const result = await FacultyServices.getSingleFacultyFromDB( id );
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty retrieved successfully',
+    message: 'Faculty is retrieved succesfully',
     data: result,
   });
 });
+
+const getAllFaculties = catchAsync(async (req, res) => {
+  const result = await FacultyServices.getAllFacultiesFromDB(req.query);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Faculties are retrieved succesfully',
+    data: result,
+  });
+});
+
 const updateFaculty = catchAsync(async (req, res) => {
-  const { facultyId } = req.params;
+  const { id } = req.params;
   const { faculty } = req.body;
-  const result = await FacultyServices.updateFacultyIntoDB(facultyId, faculty);
+  const result = await FacultyServices.updateFacultyIntoDB( id , faculty);
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty Updated successfully',
+    message: 'Faculty is updated succesfully',
     data: result,
   });
 });
+
 const deleteFaculty = catchAsync(async (req, res) => {
-  const { facultyId } = req.params;
-  await FacultyServices.deleteFacultyFromDB(facultyId);
+  const {  id } = req.params;
+  const result = await FacultyServices.deleteFacultyFromDB(id);
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty deleted successfully',
-    data: null,
+    message: 'Faculty is deleted succesfully',
+    data: result,
   });
 });
 
 export const FacultyControllers = {
   getAllFaculties,
   getSingleFaculty,
-  updateFaculty,
   deleteFaculty,
+  updateFaculty,
 };
--- src/app/modules/faculty/faculty.validation.ts
+++ src/app/modules/faculty/faculty.validation.ts
@@ -1,61 +1,38 @@
 import { z } from 'zod';
+import { BloodGroup, Gender } from './faculty.constant';
 
-// Define UserName schema
-const userNameValidationSchema = z.object({
+const createUserNameValidationSchema = z.object({
   firstName: z
     .string()
-    .max(20, { message: 'First Name length cannot be longer than 20' })
-    .min(1, { message: 'First Name is required' })
-    .refine(
-      (value) => {
-        const trimmedValue = value.trim();
-        const firstNameStr =
-          trimmedValue.charAt(0).toUpperCase() + trimmedValue.slice(1);
-        return value === firstNameStr;
-      },
-      { message: 'First Name must start with a capital letter' },
-    ),
-  middleName: z.string().optional(),
-  lastName: z
-    .string()
-    .min(1, { message: 'Last Name is required' })
-    .refine((value) => /^[A-Za-z]+$/.test(value.trim()), {
-      message: 'Last Name must only contain alphabetic characters',
+    .min(1)
+    .max(20)
+    .refine((value) => /^[A-Z]/.test(value), {
+      message: 'First Name must start with a capital letter',
     }),
+  middleName: z.string(),
+  lastName: z.string(),
 });
 
-// Define Student schema
-const createFacultyValidationSchema = z.object({
+export const createFacultyValidationSchema = z.object({
   body: z.object({
-    password: z
-      .string()
-      .max(20, { message: 'Pasword can not be more than 20 characters' }),
+    password: z.string().max(20),
     faculty: z.object({
-      name: userNameValidationSchema,
-      gender: z.enum(['male', 'female', 'other'], {
-        message: 'Gender is not valid',
-      }),
+      designation: z.string(),
+      name: createUserNameValidationSchema,
+      gender: z.enum([...Gender] as [string, ...string[]]),
       dateOfBirth: z.string().optional(),
-      email: z.string().email({ message: 'Invalid email address' }),
-      contactNo: z.string().min(1, { message: 'Contact No is required' }),
-      emergencyContactNo: z
-        .string()
-        .min(1, { message: 'Emergency Contact No is required' }),
-      bloodGroup: z
-        .enum(['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-'])
-        .optional(),
-      presentAddress: z
-        .string()
-        .min(1, { message: 'Present Address is required' }),
-      permanentAddress: z
-        .string()
-        .min(1, { message: 'Permanent Address is required' }),
-
-      profileImg: z.string().optional(),
+      email: z.string().email(),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      bloogGroup: z.enum([...BloodGroup] as [string, ...string[]]),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
       academicDepartment: z.string(),
+      profileImg: z.string(),
     }),
   }),
 });
+
 const updateUserNameValidationSchema = z.object({
   firstName: z.string().min(1).max(20).optional(),
   middleName: z.string().optional(),
@@ -65,15 +42,14 @@ const updateUserNameValidationSchema = z.object({
 export const updateFacultyValidationSchema = z.object({
   body: z.object({
     faculty: z.object({
+      designation: z.string().optional(),
       name: updateUserNameValidationSchema,
-      gender: z.enum(['male', 'female', 'other']).optional(),
+      gender: z.enum([...Gender] as [string, ...string[]]).optional(),
       dateOfBirth: z.string().optional(),
       email: z.string().email().optional(),
       contactNo: z.string().optional(),
       emergencyContactNo: z.string().optional(),
-      bloogGroup: z
-        .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
-        .optional(),
+      bloogGroup: z.enum([...BloodGroup] as [string, ...string[]]).optional(),
       presentAddress: z.string().optional(),
       permanentAddress: z.string().optional(),
       profileImg: z.string().optional(),
@@ -82,7 +58,7 @@ export const updateFacultyValidationSchema = z.object({
   }),
 });
 
-export const facultyValidations = {
+export const studentValidations = {
   createFacultyValidationSchema,
   updateFacultyValidationSchema,
 };
